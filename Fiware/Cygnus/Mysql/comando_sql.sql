6d5b08c37c8a9fe516cb573a0c99b8f8fb9203e5
Alisson Lima <alissonlima.igt@gmail.com>
2017-05-22 15:47:30 -0300

    Update comando_sql.sql

--- Fiware/Cygnus/Mysql/comando_sql.sql
+++ Fiware/Cygnus/Mysql/comando_sql.sql
@@ -15,18 +15,17 @@ temperatura
 temperatura_md
 umidade
 umidade_md
-monoxido
-monoxido_md
+dioxido
+dioxido_md
 amostra
 ]
   
-CREATE TABLE IF NOT EXISTS `<NOME_COMEIA_Colmeia` (
+CREATE TABLE IF NOT EXISTS `Colmeia2_Colmeia` (
   `amostra` integer primary key AUTO_INCREMENT,
-  `temperatura` FLOAT NULL, `umidade` FLOAT NULL, 
+  `temperatura` FLOAT NULL, `umidade` FLOAT NULL, `dioxido` FLOAT NULL,
   `som` FLOAT NULL, `data` VARCHAR(45) NULL,
-  `dioxido` FLOAT NULL,
   `hora` VARCHAR(45) NULL, `entityId` VARCHAR(45) NOT NULL,  
   `fiwareServicePath` VARCHAR(45) NULL, `entityType` VARCHAR(45) NULL,   
   `recvTime` TEXT(45) NULL, `temperatura_md` TEXT(45) NULL,   
-  `umidade_md` TEXT(45) NULL, `som_md` TEXT(45) NULL,
-`data_md` TEXT(45) NULL, `hora_md` TEXT(45) NULL, `dioxido_md` TEXT(45) NULL);
+  `umidade_md` TEXT(45) NULL, `dioxido_md` TEXT(45) NULL,`som_md` TEXT(45) NULL, 
+`data_md` TEXT(45) NULL, `hora_md` TEXT(45) NULL );
